76ba06ef5d7967b916d691577df82bcf92d3d393
Matty <matty258987@gmail.com>
2022-01-22 21:07:33 +0000

    back to it

--- src/main/java/com/example/javafxtesting/HelloApplication.java
+++ src/main/java/com/example/javafxtesting/HelloApplication.java
@@ -1,14 +1,12 @@
 package com.example.javafxtesting;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloApplication extends Application {
 
@@ -17,28 +15,11 @@ public class HelloApplication extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-
-        Group root = new Group();
-        Scene scene = new Scene(root, 690, 420, Color.AQUA);
-
-        Text text = new Text();
-        text.setText("WAGWAN MY G");
-        text.setX(69);
-        text.setY(69);
-        text.setFont(Font.font("Verdana", 69));
-
-        Line line = new Line();
-        line.setStartX(120);
-        line.setStartY(120);
-        line.setEndX(480);
-        line.setEndY(240);
-        line.setStrokeWidth(5);
-        line.setStroke(Color.CORNSILK);
-
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage pStage) throws IOException {
+        Parent root = FXMLLoader.load((HelloApplication.class.getResource("hello-view.fxml")));
+        Scene scene = new Scene(root, 320, 240);
+        pStage.setTitle("Hello!");
+        pStage.setScene(scene);
+        pStage.show();
     }
 }
