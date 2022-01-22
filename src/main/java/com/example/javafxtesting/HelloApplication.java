a7ce2bcaa3c1f6e1a03304d2f89ff60206275fc9
Matty <matty258987@gmail.com>
2022-01-22 20:30:43 +0000

    basic stage options

--- src/main/java/com/example/javafxtesting/HelloApplication.java
+++ src/main/java/com/example/javafxtesting/HelloApplication.java
@@ -1,6 +1,10 @@
 package com.example.javafxtesting;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
@@ -12,5 +16,18 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.AQUA);
+
+        stage.setTitle("L33T H@X");
+        stage.setWidth(420);
+        stage.setHeight(69);
+        stage.setResizable(false);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("There is no escape. (pressQ)");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
