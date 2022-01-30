f95ed2754e9acf9e8a6db51fdad61648a4b0cc44
Matty <matty258987@gmail.com>
2022-01-30 16:13:29 +0000

    add scene switching functionality

--- src/main/java/com/example/javafxtesting/HelloController.java
+++ src/main/java/com/example/javafxtesting/HelloController.java
@@ -2,9 +2,20 @@ package com.example.javafxtesting;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private Circle middleCircle;
@@ -29,5 +40,29 @@ public class HelloController {
     public void right(ActionEvent e) {
         System.out.println("RIGHT");
         middleCircle.setCenterX(circleX += 10);
+    }
+
+    public void switchToScene1(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSceneHome(ActionEvent e) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
