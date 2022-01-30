97062ac848c66bf361a994c2ff09c7e7b8b5d442
Matty <matty258987@gmail.com>
2022-01-30 19:02:33 +0000

    add logout button

--- src/main/java/com/example/javafxtesting/WelcomeController.java
+++ src/main/java/com/example/javafxtesting/WelcomeController.java
@@ -1,7 +1,16 @@
 package com.example.javafxtesting;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class WelcomeController {
     @FXML
@@ -9,5 +18,14 @@ public class WelcomeController {
 
     public void displayName(String username) {
         txtNameLabel.setText("Welcome, " + username);
+    }
+
+    public void logout(ActionEvent e) throws IOException {
+        txtNameLabel.setText("Welcome, ");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
