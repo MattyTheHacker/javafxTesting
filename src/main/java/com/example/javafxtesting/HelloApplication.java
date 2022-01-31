fe8fa6fc92dbc398f292f652c0832306d9d6ee14
Matty <matty258987@gmail.com>
2022-01-31 17:33:02 +0000

    fix formatting

--- src/main/java/com/example/javafxtesting/HelloApplication.java
+++ src/main/java/com/example/javafxtesting/HelloApplication.java
@@ -11,7 +11,6 @@ import javafx.stage.Stage;
 import java.util.Objects;
 
 public class HelloApplication extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
